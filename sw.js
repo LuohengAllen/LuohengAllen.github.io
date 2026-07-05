bfce6f4798842ac39ae414dbca061472f71e33a5
Allen <gefengkai@163.com>
2026-07-06 04:28:59 +0800

    Site updated: 2026-07-06 04:28:45

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1783114020855";const preCache = ["/images/taichi.png","/images/bg.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1783283318039";const preCache = ["/images/taichi.png","/images/bg.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
